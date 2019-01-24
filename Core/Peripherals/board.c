d8838cd6e9642393d49abb0782afba2177dcc3ae
edmching <edmching@hotmail.com>
2019-01-24 10:58:52 -0800

    init commit

---
+++ Core/Peripherals/board.c
@@ -0,0 +1,37 @@
+/*
+ * board.c
+ *
+ *  Created on: Jan 17, 2019
+ *      Author: edmch
+ */
+#include "board.h"
+
+void BoardInit(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  // GPIO Ports Clock Enable
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  //Configure GPIO pin Output Level
+  HAL_GPIO_WritePin(LD2_GPIO_PORT, LD2_PIN, GPIO_PIN_RESET);
+
+  //Configure GPIO pin : B1_PIN
+  GPIO_InitStruct.Pin = B1_PIN;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_PORT, &GPIO_InitStruct);
+
+  //Configure GPIO pins : LD2_PIN
+  GPIO_InitStruct.Pin = LD2_PIN;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_PORT, &GPIO_InitStruct);
+
+
+
+}
---
+++ Core/Peripherals/steppermotor.h
@@ -0,0 +1,40 @@
+/*
+ * steppermotor.h
+ *
+ *  Created on: Jan 15, 2019
+ *      Author: edmch
+ */
+
+#ifndef STEPPERMOTOR_H_
+#define STEPPERMOTOR_H_
+
+#include "stm32f4xx_hal.h"
+
+#define STEPMOTOR_PORT_IN			GPIOB
+
+#define STEPMOTOR_PIN_IN1			GPIO_PORT_D_PIN_3
+#define STEPMOTOR_PIN_IN2			GPIO_PORT_D_PIN_4
+#define STEPMOTOR_PIN_IN3			GPIO_PORT_D_PIN_5
+#define STEPMOTOR_PIN_IN4			GPIO_PORT_D_PIN_6
+
+#define GPIO_PORT_D_PIN_6			GPIO_PIN_10
+#define GPIO_PORT_D_PIN_5			GPIO_PIN_4
+#define GPIO_PORT_D_PIN_4			GPIO_PIN_5
+#define GPIO_PORT_D_PIN_3			GPIO_PIN_3
+
+#define NUM_STEPS_360_DEG			512
+#define FULL_ROTATATION_IN_DEG		360.0
+
+//uint8_t wave_CW[4] = {0b1000, 0b0100, 0b0010, 0b0001};
+//uint8_t wave_CCW[4] = {0b0001, 0b0100, 0b0010, 0b0001};
+
+typedef enum {CW,CCW} direction;
+
+void StepmotorGPIOInit(void);
+void StepmotorWaveDriveCW (void);
+void StepmotorWaveDriveCCW (void);
+void StepmotorFullDriveCW (void);
+void StepmotorHalfDriveCW (void);
+void StepmotorMoveAngle(int angle, direction _direction);
+
+#endif /* STEPPERMOTOR_H_ */
